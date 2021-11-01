8eb809695aa9d27e8003253a0bbdd27c4028c893
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-11-01 09:11:19 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string whatAge = Console.ReadLine();
+            int whaAge = Convert.ToInt32(whatAge);
+            Console.ReadLine();
+
+
+        }
+    }
+}
