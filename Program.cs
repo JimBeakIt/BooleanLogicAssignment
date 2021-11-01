cb0b2caa559b5bdddda8e50ea76126825a479f7b
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-11-01 09:36:09 +0000

    update

--- Program.cs
+++ Program.cs
@@ -15,6 +15,20 @@ namespace BooleanLogicAssignment
             int whaAge = Convert.ToInt32(whatAge);
             Console.ReadLine();
 
+            Console.WriteLine("Have you ever had a DUI violation? Please enter 'True' or 'False'.");
+            string duiV = Console.ReadLine();
+            bool dV = Convert.ToBoolean(duiV);
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speedT = Console.ReadLine();
+            int speT = Convert.ToInt32(speedT);
+            Console.ReadLine();
+
+            Console.WriteLine("Qualified?");
+            bool result = (whaAge > 15 && dV == false && speT < 4);
+            Console.WriteLine(result);
+            Console.ReadLine();
 
         }
     }
